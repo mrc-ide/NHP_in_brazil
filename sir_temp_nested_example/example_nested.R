d1b2025bf2717e82fceea617e0d776ee3433edd5
KeithJF82 <keith.fraser@imperial.ac.uk>
2024-01-25 12:39:51 +0000

    Update example_nested.R

--- sir_temp_nested_example/example_nested.R
+++ sir_temp_nested_example/example_nested.R
@@ -46,11 +46,9 @@ filter <- mcstate::particle_filter$new(data = data,
                                        seed = 42L)
 
 # now for the fitting
-
-beta <-   mcstate::pmcmc_parameter("beta", 0.2, min = 0)
-gamma <-   mcstate::pmcmc_parameter("gamma", 0.1, min = 0, prior = function(p)
+gamma <-   mcstate::pmcmc_parameter("gamma", 0.5, min = 0, prior = function(p)
     dgamma(p, shape = 1, scale = 0.2, log = TRUE))
-temp_scale <-   mcstate::pmcmc_varied_parameter("temp_scale", c(1e-5, 1e-5), 
+temp_scale <-   mcstate::pmcmc_varied_parameter("temp_scale", c(1e-4, 1e-4), 
                                   populations = c("a", "b"),min=0) #this changes between populations
 
 transform <- function(theta) {
@@ -60,15 +58,14 @@ transform <- function(theta) {
 make_transform <- function(I0) {
   function(theta) {
     list(I0 =I0, #this runs everything with standardised I0 inital infecion
-         beta=theta[["beta"]],
          gamma=theta[["gamma"]],
          temp_scale=theta[["temp_scale"]])
   }
 }
 
 #proposal distributions
-proposal_fixed <- (1e-2) ^ 2 * diag(2) / 2 # this is for the proposal distribution
-colnames(proposal_fixed) <- c("beta", "gamma")
+proposal_fixed <- (1e-2) ^ 2 * diag(1) / 2 # this is for the proposal distribution
+colnames(proposal_fixed) <- c("gamma")
 proposal_varied <- array(c(0.0001, 0.000095), c(1, 1, 2),
                          dimnames = list("temp_scale", "temp_scale", c("a", "b")))
 
@@ -76,7 +73,7 @@ transform <- make_transform(10) #then this bounds I0 as 10
 
 #getting proposal priors and starting parameters in the correct format
 mcmc_pars <- mcstate::pmcmc_parameters_nested$new(
-  parameters = list(beta = beta, gamma = gamma, temp_scale=temp_scale),
+  parameters = list(gamma = gamma, temp_scale=temp_scale),
   proposal_varied = proposal_varied,
   proposal_fixed = proposal_fixed,
   populations = c("a", "b")
@@ -92,29 +89,22 @@ control <- mcstate::pmcmc_control(
 samples <- mcstate::pmcmc(mcmc_pars, filter, control = control)
 
 #samples
-plot(samples$probabilities[100:1000, "log_posterior",1], type = "s",
+par(mfrow = c(1, 2))
+plot(samples$probabilities[1:1000, "log_posterior",1], type = "s",
      xlab = "Sample", ylab = "Log posterior") #this shows the chain
-plot(samples$probabilities[100:1000, "log_posterior",2], type = "s",
+plot(samples$probabilities[1:1000, "log_posterior",2], type = "s",
      xlab = "Sample", ylab = "Log posterior") #this shows the chain
 
 # plot result
 t <- 0:100
+observed_A=df_tidy[df_tidy$population == "A", ]
+observed_B=df_tidy[df_tidy$population == "B", ]
+output_A=t(samples$trajectories$state[6, 1, , ])
+output_B=t(samples$trajectories$state[6, 2, , ])
 par(mfrow = c(1, 2))
-matplot(t, t(samples$trajectories$state[2, , 1, ]), type = "l", lty = 1,
-        col = "#00000011", xlab =  "Day", ylab = "Infected",
-        main = "Daily Infected in Population A")
-matplot(t, t(samples$trajectories$state[2, , 2, ]),
-        col = "#00000011", xlab =  "Day", ylab = "Infected", type = "l",
-        lty = 1, main = "Daily Infected in Population B") 
-
-par(mfrow = c(1, 2))
-matplot(t[-1], diff(t(samples$trajectories$state[4, , 1, ])), type = "l",
-        lty = 1, col = "#00000005", xlab = "Day", ylab = "Daily incidence",
-        main = "Daily Incidence in Population A")
-points(cases ~ day, df_tidy[df_tidy$population == "A", ], col = "blue",
-       pch = 19)
-matplot(t[-1], diff(t(samples$trajectories$state[4, , 2, ])), type = "l",
-        lty = 1, col = "#00000005", xlab = "Day", ylab = "Daily incidence",
-        main = "Daily Incidence in Population B")
-points(cases ~ day, df_tidy[df_tidy$population == "B", ], col = "blue",
-       pch = 19)
+matplot(x=t, y=output_A, type = "l", lty = 1, col = "#00000005", xlab = "Day", ylab = "Daily incidence",
+        main = "Daily Incidence - Pop. A",ylim=c(0,max(observed_A$cases,output_A)))
+matplot(x=observed_A$day,observed_A$cases,type="p",pch=19,col="blue",add=TRUE)
+matplot(x=t, y=output_B, type = "l", lty = 1, col = "#00000005", xlab = "Day", ylab = "Daily incidence",
+        main = "Daily Incidence - Pop. B",ylim=c(0,max(observed_B$cases,output_B)))
+matplot(x=observed_B$day,observed_B$cases,type="p",pch=19,col="blue",add=TRUE)
