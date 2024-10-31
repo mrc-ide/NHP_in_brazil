a2fd12e25ff2465f332368371b90cf0418b465b3
KeithJF82 <keith.fraser@imperial.ac.uk>
2024-10-31 14:32:02 +0000

    Updated multi-genera model

--- multiple_genera/demo_multiple_genera.R
+++ multiple_genera/demo_multiple_genera.R
@@ -1,27 +1,28 @@
 library(odin.dust)
 
-model <- odin.dust::odin_dust("multiple_genera/sir_multiple_genera.R")
+model <- odin.dust::odin_dust("multiple_genera/seir_multiple_genera.R")
 
 dt=1 #Time increment in days
-beta=0.075 #Beta parameter governing transmission from infectious individuals
+n_t_pts=7300 #Number of time points to run (beta and FOI_in must have lengths equal to n_t_pts)
+beta=rep(0.9,n_t_pts) #Beta parameter governing transmission from infectious individuals, at each time point
 t_incubation=5 #Incubation time in vectors (currently no vector species separation)
 t_latent=c(5,5) #Latent period in NHPs by genus
 t_infectious=c(5,5)  #Infectious period in NHPs by genus
-FOI_in=1.0e-9 #Force of infection (per day) for importation of infections from outside modelled populations
-R_dP=c(100,200)/365 #Population turnover (birth rate/death rate per day) by genus (assumed stable population)
+ifr=c(0.1,0.5) #Infection fatality rate by genus
+FOI_in=rep(1.0e-5,n_t_pts) #Force of infection (per day) for importation of infections, at each time point
+mu=c(0.1,0.1)/365 #Birth/death rate per unit population per day (1/lifespan in years divided by 365) by genus
 n_gen=2 #Number of genera (vectors of input values must have lengths equal to n_gen)
 S_0=c(1000,1000) #Initial number of susceptible individuals by genus
 E_0=c(0,0) #Initial number of exposed individuals by genus
 I_0=c(0,0) #Initial number of infectious individuals by genus
 R_0=c(0,0) #Initial number of recovered individuals by genus
-pars <- list(dt=dt,beta=beta,t_incubation=t_incubation,t_latent=t_latent,t_infectious=t_infectious,
-             FOI_in=FOI_in,R_dP=R_dP,n_gen=n_gen,S_0=S_0,E_0=E_0,I_0=I_0,R_0=R_0)
+pars <- list(dt=dt,n_t_pts=n_t_pts,beta=beta,t_incubation=t_incubation,t_latent=t_latent,t_infectious=t_infectious,
+             ifr=ifr,FOI_in=FOI_in,mu=mu,n_gen=n_gen,S_0=S_0,E_0=E_0,I_0=I_0,R_0=R_0)
 
-tmax=36500
 n_particles=1
 run <- model$new(pars = pars, time=1, n_particles=n_particles, n_threads=1, deterministic=TRUE)
-output=array(NA,dim=c(tmax,n_particles,12))
-for(i in 1:tmax){
+output=array(NA,dim=c(pars$n_t_pts,n_particles,2+(6*pars$n_gen)))
+for(i in 1:pars$n_t_pts){
   output[i,,]=run$run(i)
 }
 
@@ -31,7 +32,13 @@ S=output[,1,2+c(1:pars$n_gen)]
 E=output[,1,2+pars$n_gen+c(1:pars$n_gen)]
 I=output[,1,2+(2*pars$n_gen)+c(1:pars$n_gen)]
 R=output[,1,2+(3*pars$n_gen)+c(1:pars$n_gen)]
-C=output[,1,2+(3*pars$n_gen)+c(1:pars$n_gen)]
+C=output[,1,2+(4*pars$n_gen)+c(1:pars$n_gen)]
+D=output[,1,2+(5*pars$n_gen)+c(1:pars$n_gen)]
+C_c=D_c=C
+for(i in 1:pars$n_gen){
+  C_c[,i]=cumsum(C[,i])
+  D_c[,i]=cumsum(D[,i])
+}
 
 par(mar=c(4,4,1,1))
 FOI_ticks=10^c(-8:-1)
@@ -40,9 +47,19 @@ axis(side=2,at=log(FOI_ticks),labels=FOI_ticks)
 
 par(mfrow=c(1,pars$n_gen),mar=c(4,2,1,1))
 for(i in 1:pars$n_gen){
-  matplot(x=c(1,max(time)),y=c(0,pars$S_0[i]+pars$E_0[i]+pars$I_0[i]+pars$R_0[i]),type="p",col=0,xlab="Time",ylab="")
+  matplot(x=c(1,max(time)),y=c(0,1.2*(pars$S_0[i]+pars$E_0[i]+pars$I_0[i]+pars$R_0[i])),type="p",col=0,xlab="Time",ylab="")
   matplot(x=time,y=S[,i],type="l",col=1,add=TRUE)
   matplot(x=time,y=R[,i],type="l",col=2,add=TRUE)  
-  legend("topright",legend=c("S","R"),lty=c(1,1),col=c(1,2))
+  matplot(x=time,y=S[,i]+E[,i]+I[,i]+R[,i],type="l",col=3,add=TRUE)  
+  legend("topright",legend=c("Susceptible","Recovered","Total population"),lty=c(1,1,1),col=c(1,2,3))
+}
+par(mfrow=c(1,1))
+
+par(mfrow=c(1,pars$n_gen),mar=c(4,2,1,1))
+for(i in 1:pars$n_gen){
+  matplot(x=c(1,max(time)),y=c(0,max(C_c[,i])),type="p",col=0,xlab="Time",ylab="")
+  matplot(x=time,y=C_c[,i],type="l",col=1,add=TRUE)
+  matplot(x=time,y=D_c[,i],type="l",col=2,add=TRUE)  
+  legend("topleft",legend=c("Infections","Deaths"),lty=c(1,1),col=c(1,2))
 }
 par(mfrow=c(1,1))
--- multiple_genera/seir_multiple_genera.R
+++ multiple_genera/seir_multiple_genera.R
@@ -2,26 +2,26 @@
 # with multiple genera
 
 # TO DO (to add or to include in future version):
-# -Incorporate deaths due to YF (infection fatality rate parameter)
-# -Update population modelling to allow for population extinction or recovery (look at existing ecological models)
-# -Set number of births/deaths at each time point using binomial expression like new infections, with
-# -Incorporate introductions of individuals from outside
+# -Update population modelling to allow for population extinction or recovery (look at existing ecological models, ongoing)
+# -Incorporate introductions of individuals from outside (and outward migration)
 # -Make force of infection genus dependent
 # -Add vector model including functionality for multiple vector species
-# -Make force of infection time-varying (by making beta and/or FOI_in time-varying)
 # -Make force of infection (via beta and/or FOI_in) driven by temperature/precipitation input
+# -Change model of force of infection, latency, etc. for better short-term timescale modelling
 
 dt <- user() #Time increment in days
 initial(time) <- 0 #Initial value of time in days
 update(time) <- time + dt
 
 #Parameters---------------------------------------------------------------------
-beta <- user() #beta parameter for transmission [TODO: MAKE TIME-VARYING/CLIMATE-DRIVEN]
+n_t_pts <- user() #Number of time points to be run
+beta[] <- user() #Time-varying beta parameter for transmission
 t_incubation <- user() #Length in days of yellow fever incubation period in mosquito vectors [TODO: MAKE VAR BY SPECIES]
 t_latent[] <- user() #Length in days of latent period in NHPs exposed to yellow fever
 t_infectious[] <- user() #Length of infectious period in NHPs with yellow fever
-FOI_in <- user() #Force of infection (per day) for outside introduction [TODO: MAKE TIME-VARYING/CLIMATE-DRIVEN]
-R_dP[] <- user() #Rate of population turnover by day (birth rate/death rate, assumed equal for stable population)
+ifr[] <- user() #Infection fatality rate
+FOI_in[] <- user() #Time-varying force of infection (per day) for outside introduction
+mu[] <- user() #Rate of population turnover by day (birth rate/death rate, assumed equal for stable population)
 n_gen <- user() #Number of genera to look at
 
 #Initial conditions-------------------------------------------------------------
@@ -30,33 +30,39 @@ E_0[] <- user() #Exposed NHP population by genus at start
 I_0[] <- user() #Infectious NHP population by genus at start
 R_0[] <- user() #Recovered NHP population by genus at start
 
-Pmin <- 1.0e-99 #Minimum population setting to avoid negative numbers
+Pmin <- 0.0 #Minimum population setting to avoid negative numbers
 FOI_max <- 1.0 #Upper threshold for total force of infection to avoid more infections than individuals
 rate1[1:n_gen] <- dt/(t_incubation+t_latent[i]) #Rate of transference E->I
 rate2[1:n_gen] <- dt/t_infectious[i] #Rate of transference I->R
 
-FOI_sum <-  min(FOI_max,dt*((beta*sum(I)*sum(inv_P)) + FOI_in)) #Total force of infection
+FOI_sum <-  min(FOI_max,dt*(((beta[step+1]*sum(I))/sum(P)) + FOI_in[step+1])) #Total force of infection
+births[1:n_gen] <- rbinom(as.integer(P[i]), mu[i]) #New births by genus
+deaths_nat[1:n_gen] <- rbinom(as.integer(P[i]), mu[i]) #New deaths by genus (excluding YF deaths)
 E_new[1:n_gen] <- rbinom(as.integer(S[i]), FOI_sum) #New exposed NHPs by genus
 I_new[1:n_gen] <- E[i]*rate1[i]     #New infectious NHPs by genus
-R_new[1:n_gen] <- I[i]*rate2[i]     #New recovered NHPs by genus
-P[1:n_gen] <- S[i] + R[i] #Total NHP population by genus (excluding E+I)
+R_new0[1:n_gen] <- I[i]*rate2[i]     #Number of NHPS removed from I by genus (recovery from or death due to YF)
+R_new[1:n_gen] <- R_new0[i]*(1.0-ifr[i]) #New recovered NHPs by genus
+deaths_YF[1:n_gen] <- R_new0[i]*ifr[i]  #New deaths of NHPs by genus
+P[1:n_gen] <- S[i] + E[i] + I[i] + R[i] #Total NHP population by genus
 inv_P[1:n_gen] <- 1.0/P[i]
 
 #Updates to output values at each time increment--------------------------------
 update(FOI_total) <- FOI_sum
-update(S[1:n_gen]) <- max(Pmin,S[i] - E_new[i] + R_dP[i] - (R_dP[i]*S[i]*inv_P[i]))
-update(E[1:n_gen]) <- max(Pmin,E[i] + E_new[i] - I_new[i])
-update(I[1:n_gen]) <- max(Pmin,I[i] + I_new[i] - R_new[i])
-update(R[1:n_gen]) <- max(Pmin,R[i] + R_new[i] - (R_dP[i]*R[i]*inv_P[i]))
+update(S[1:n_gen]) <- max(Pmin,S[i] - E_new[i] + births[i] - (deaths_nat[i]*S[i]*inv_P[i]))
+update(E[1:n_gen]) <- max(Pmin,E[i] + E_new[i] - I_new[i] - (deaths_nat[i]*E[i]*inv_P[i]))
+update(I[1:n_gen]) <- max(Pmin,I[i] + I_new[i] - R_new0[i] - (deaths_nat[i]*I[i]*inv_P[i]))
+update(R[1:n_gen]) <- max(Pmin,R[i] + R_new[i] - (deaths_nat[i]*R[i]*inv_P[i]))
 update(C[1:n_gen]) <- I_new[i]
+update(D[1:n_gen]) <- deaths_YF[i]
 
 #Initial values-----------------------------------------------------------------
-initial(FOI_total) <- FOI_in
+initial(FOI_total) <- FOI_in[1]
 initial(S[1:n_gen]) <- S_0[i]
 initial(E[1:n_gen]) <- E_0[i]
 initial(I[1:n_gen]) <- I_0[i]
 initial(R[1:n_gen]) <- R_0[i]
 initial(C[1:n_gen]) <- 0
+initial(D[1:n_gen]) <- 0
 
 #Dimensions---------------------------------------------------------------------
 dim(S) <- n_gen
@@ -64,9 +70,15 @@ dim(E) <- n_gen
 dim(I) <- n_gen
 dim(R) <- n_gen
 dim(C) <- n_gen
+dim(D) <- n_gen
+
+dim(births) <- n_gen
+dim(deaths_nat) <- n_gen
 dim(E_new) <- n_gen
 dim(I_new) <- n_gen
+dim(R_new0) <- n_gen
 dim(R_new) <- n_gen
+dim(deaths_YF) <- n_gen
 
 dim(P) <- n_gen
 dim(inv_P) <- n_gen
@@ -76,9 +88,11 @@ dim(E_0) <- n_gen
 dim(I_0) <- n_gen
 dim(R_0) <- n_gen
 
+dim(beta) <- n_t_pts
+dim(FOI_in) <- n_t_pts
 dim(t_latent) <- n_gen
 dim(t_infectious) <- n_gen
-dim(R_dP) <- n_gen
+dim(ifr) <- n_gen
+dim(mu) <- n_gen
 dim(rate1) <- n_gen
 dim(rate2) <- n_gen
-
