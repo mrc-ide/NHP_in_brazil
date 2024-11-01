14dcb5cd1d5090493524603692e5d1db847c4c9d
KeithJF82 <keith.fraser@imperial.ac.uk>
2024-11-01 13:45:01 +0000

    Updated multiple-genera model and example

--- multiple_genera/demo_multiple_genera.R
+++ multiple_genera/demo_multiple_genera.R
@@ -1,9 +1,9 @@
-# Demonstration of SEIR non-human primate model with multiple genera
-# Epidemiological (epi) parameters calculated linearly from temperature and precipitation:
+# This is a demonstration of the new SEIR non-human primate model with functionality for multiple genera
+# Epidemiological (epi) parameters are calculated linearly from temperature and precipitation:
 # - beta - standard beta parameter governing new infections due to infectious individuals
 # - FOI_in - daily force of infection for new infections imported from outside
-# Epi parameters calculated from linear combination of temperature and precipitation using coefficients
-# Future version will incorporate non-linear calculation of epi parameters from temperature and precipitation
+# Epi parameters are calculated via linear combination of temperature and precipitation using coefficients
+# A future version will incorporate non-linear calculation of epi parameters from temperature and precipitation
 
 #Load odin.dust and compile model-----------------------------------------------
 library(odin.dust)
@@ -11,28 +11,32 @@ model <- odin.dust::odin_dust("multiple_genera/seir_multiple_genera.R")
 
 #Set up parameters--------------------------------------------------------------
 dt=1 #Time increment in days
-n_t_pts=7300 #Number of time points to run (beta and FOI_in must have lengths equal to n_t_pts)
-temp=rep(25.0,n_t_pts) #Temperature at each time point, to be used to calculate epi parameters beta and FOI_in
-precip=rep(100,n_t_pts) #Precipitation at each time point, to be used to calculate epi parameters beta and FOI_in
-c_beta_temp=1e-4 #Coefficient used to calculate temperature component of beta parameter
+c_beta_temp=1e-5 #Coefficient used to calculate temperature component of beta parameter
 c_beta_precip=1e-6 #Coefficient used to calculate precipitation component of beta parameter
-c_FOI_in_temp=1e-4 #Coefficient used to calculate temperature component of FOI_in parameter
+c_FOI_in_temp=1e-5 #Coefficient used to calculate temperature component of FOI_in parameter
 c_FOI_in_precip=1e-6 #Coefficient used to calculate precipitation component of FOI_in parameter
 t_incubation=5 #Incubation time in vectors (currently no vector species separation)
+#
+n_t_pts=7300 #Number of time points to run (vectors beta and FOI_in must have lengths equal to n_t_pts)
+temp=rep(25.0,n_t_pts) #Temperature at each time point, to be used to calculate epi parameters beta and FOI_in
+precip=rep(100,n_t_pts) #Precipitation at each time point, to be used to calculate epi parameters beta and FOI_in
+#
+n_gen=2 #Number of genera (vectors t_latent, t_infectious, S_0, E_0, I_0, R_0 must have lengths equal to n_gen)
 t_latent=c(5,5) #Latent period in NHPs by genus
 t_infectious=c(5,5)  #Infectious period in NHPs by genus
 ifr=c(0.1,0.5) #Infection fatality rate by genus
 mu=c(0.1,0.1)/365 #Birth/death rate per unit population per day (1/lifespan in years divided by 365) by genus
-n_gen=2 #Number of genera (vectors of input values must have lengths equal to n_gen)
 S_0=c(1000,1000) #Initial number of susceptible individuals by genus
 E_0=c(0,0) #Initial number of exposed individuals by genus
 I_0=c(0,0) #Initial number of infectious individuals by genus
 R_0=c(0,0) #Initial number of recovered individuals by genus
+#
 pars <- list(dt = dt, n_t_pts = n_t_pts, temp = temp, precip = precip, 
              c_beta_temp = c_beta_temp, c_beta_precip = c_beta_precip, c_FOI_in_temp = c_FOI_in_temp, c_FOI_in_precip = c_FOI_in_precip, 
              t_incubation = t_incubation, t_latent = t_latent, t_infectious = t_infectious, 
              ifr = ifr, mu = mu, n_gen = n_gen, S_0 = S_0, E_0 = E_0, I_0 = I_0, R_0 = R_0)
 
+#Run model in deterministic mode with 1 particle--------------------------------
 n_particles=1
 run <- model$new(pars = pars, time=1, n_particles=n_particles, n_threads=1, deterministic=TRUE)
 output=array(NA,dim=c(pars$n_t_pts,n_particles,2+(6*pars$n_gen)))
@@ -40,25 +44,28 @@ for(i in 1:pars$n_t_pts){
   output[i,,]=run$run(i)
 }
 
-time=output[,1,1]
-FOI=output[,1,2]
-S=output[,1,2+c(1:pars$n_gen)]
-E=output[,1,2+pars$n_gen+c(1:pars$n_gen)]
-I=output[,1,2+(2*pars$n_gen)+c(1:pars$n_gen)]
-R=output[,1,2+(3*pars$n_gen)+c(1:pars$n_gen)]
-C=output[,1,2+(4*pars$n_gen)+c(1:pars$n_gen)]
-D=output[,1,2+(5*pars$n_gen)+c(1:pars$n_gen)]
-C_c=D_c=C
+#Organize outputs---------------------------------------------------------------
+time=output[,1,1] #Time in days
+FOI=output[,1,2] #Total calculated force of infection at each time point
+S=output[,1,2+c(1:pars$n_gen)] #Susceptible population by genus at each time point
+E=output[,1,2+pars$n_gen+c(1:pars$n_gen)] #Exposed population by genus at each time point
+I=output[,1,2+(2*pars$n_gen)+c(1:pars$n_gen)] #Infectious population by genus at each time point
+R=output[,1,2+(3*pars$n_gen)+c(1:pars$n_gen)] #Recovered population by genus at each time point
+C=output[,1,2+(4*pars$n_gen)+c(1:pars$n_gen)] #New infections by genus at each time point
+D=output[,1,2+(5*pars$n_gen)+c(1:pars$n_gen)] #Deaths by genus at each time point
+C_c=D_c=C #Cumulative infections and deaths
 for(i in 1:pars$n_gen){
   C_c[,i]=cumsum(C[,i])
   D_c[,i]=cumsum(D[,i])
 }
 
-par(mar=c(4,4,1,1))
+#Plot force of infection over time on logarithmic scale-------------------------
+par(mar=c(4,4,1,1),mfrow=c(1,1))
 FOI_ticks=10^c(-8:-1)
 matplot(x=time,y=log(FOI),type="l",col=1,xlab="Time",ylab="Total force of infection",yaxt="n")
 axis(side=2,at=log(FOI_ticks),labels=FOI_ticks)
 
+#Plot S, R and total population over time on multiple graphs (1 graph per genus)
 par(mfrow=c(1,pars$n_gen),mar=c(4,2,1,1))
 for(i in 1:pars$n_gen){
   matplot(x=c(1,max(time)),y=c(0,1.2*(pars$S_0[i]+pars$E_0[i]+pars$I_0[i]+pars$R_0[i])),type="p",col=0,xlab="Time",ylab="")
@@ -69,6 +76,7 @@ for(i in 1:pars$n_gen){
 }
 par(mfrow=c(1,1))
 
+#Plot cumulative infections and deaths over time on multiple graphs (1 graph per genus)
 par(mfrow=c(1,pars$n_gen),mar=c(4,2,1,1))
 for(i in 1:pars$n_gen){
   matplot(x=c(1,max(time)),y=c(0,max(C_c[,i])),type="p",col=0,xlab="Time",ylab="")
--- multiple_genera/seir_multiple_genera.R
+++ multiple_genera/seir_multiple_genera.R
@@ -4,64 +4,63 @@
 # TO DO (to add or to include in future version):
 # -Update population modelling to allow for population extinction or recovery (look at existing ecological models, ongoing)
 # -Incorporate introductions of individuals from outside (and outward migration)
-# -Add vector model including functionality for multiple vector species
-# -Change model of force of infection, latency, etc. for better short-term timescale modelling
+# -Add vector model including functionality for multiple vector species (framework in place, need parameters)
+# -Change model of force of infection, latency, etc. for better short-term timescale modelling and better 
+#  modelling of multiple interacting populations (Diekmann et al 2010)
 # -Make coefficients governing calculation of epi parameters from temp/precip genus dependent
 # -Make calculation of epi parameters from temp/precip non-linear
 
-dt <- user() #Time increment in days
-initial(time) <- 0 #Initial value of time in days
-update(time) <- time + dt
-
 #Parameters---------------------------------------------------------------------
-n_t_pts <- user() #Number of time points to be run
-temp[] <- user() #Time-varying temperature used to calculate beta and FOI_in
-precip[] <- user() #Time-varying precipitation used to calculate beta and FOI_in
+dt <- user() #Time increment in days
 c_beta_temp <- user() #Coefficient used to calculate temperature component of beta parameter
 c_beta_precip <- user() #Coefficient used to calculate precipitation component of beta parameter
 c_FOI_in_temp <- user() #Coefficient used to calculate temperature component of FOI_in parameter
 c_FOI_in_precip <- user() #Coefficient used to calculate precipitation component of FOI_in parameter
 t_incubation <- user() #Length in days of yellow fever incubation period in mosquito vectors [TODO: MAKE VAR BY SPECIES]
-t_latent[] <- user() #Length in days of latent period in NHPs exposed to yellow fever
-t_infectious[] <- user() #Length of infectious period in NHPs with yellow fever
-ifr[] <- user() #Infection fatality rate
-mu[] <- user() #Rate of population turnover by day (birth rate/death rate, assumed equal for stable population)
-n_gen <- user() #Number of genera to look at
-
-#Initial conditions-------------------------------------------------------------
+#
+n_t_pts <- user() #Number of time points to be run
+temp[] <- user() #Time-varying temperature used to calculate beta and FOI_in
+precip[] <- user() #Time-varying precipitation used to calculate beta and FOI_in
+#
+n_gen <- user() #Number of genera
+t_latent[] <- user() #Length in days of latent period in NHPs exposed to yellow fever, by genus
+t_infectious[] <- user() #Length of infectious period in NHPs with yellow fever, by genus
+ifr[] <- user() #Infection fatality rate by genus
+mu[] <- user() #Rate of population turnover by day (birth rate/death rate, assumed equal for stable population) by genus
 S_0[] <- user() #Susceptible NHP population by genus at start
 E_0[] <- user() #Exposed NHP population by genus at start
 I_0[] <- user() #Infectious NHP population by genus at start
 R_0[] <- user() #Recovered NHP population by genus at start
 
+#Additional/derived values (constant)
 Pmin <- 0.0 #Minimum population setting to avoid negative numbers
 FOI_max <- 1.0 #Upper threshold for total force of infection to avoid more infections than individuals
 rate1[1:n_gen] <- dt/(t_incubation+t_latent[i]) #Rate of transference E->I
 rate2[1:n_gen] <- dt/t_infectious[i] #Rate of transference I->R
 
+#Derived values (variable)
 beta <- (c_beta_temp*temp[step+1])+(c_beta_precip*precip[step+1]) #Time-varying beta parameter for transmission
 FOI_in <- (c_FOI_in_temp*temp[step+1])+(c_FOI_in_precip*precip[step+1]) #Time-varying force of infection (per day) for outside introduction
 FOI_sum <-  min(FOI_max,dt*(((beta*sum(I))/sum(P)) + FOI_in)) #Total force of infection
-births[1:n_gen] <- rbinom(as.integer(P[i]), mu[i]) #New births by genus
-deaths_nat[1:n_gen] <- rbinom(as.integer(P[i]), mu[i]) #New deaths by genus (excluding YF deaths)
 E_new[1:n_gen] <- rbinom(as.integer(S[i]), FOI_sum) #New exposed NHPs by genus
 I_new[1:n_gen] <- E[i]*rate1[i]     #New infectious NHPs by genus
 R_new0[1:n_gen] <- I[i]*rate2[i]     #Number of NHPS removed from I by genus (recovery from or death due to YF)
 R_new[1:n_gen] <- R_new0[i]*(1.0-ifr[i]) #New recovered NHPs by genus
-deaths_YF[1:n_gen] <- R_new0[i]*ifr[i]  #New deaths of NHPs by genus
+deaths_YF[1:n_gen] <- R_new0[i]*ifr[i]  #New deaths of NHPs by genus due to YF
 P[1:n_gen] <- S[i] + E[i] + I[i] + R[i] #Total NHP population by genus
-inv_P[1:n_gen] <- 1.0/P[i]
 
 #Updates to output values at each time increment--------------------------------
+update(time) <- time + dt
 update(FOI_total) <- FOI_sum
-update(S[1:n_gen]) <- max(Pmin,S[i] - E_new[i] + births[i] - (deaths_nat[i]*S[i]*inv_P[i]))
-update(E[1:n_gen]) <- max(Pmin,E[i] + E_new[i] - I_new[i] - (deaths_nat[i]*E[i]*inv_P[i]))
-update(I[1:n_gen]) <- max(Pmin,I[i] + I_new[i] - R_new0[i] - (deaths_nat[i]*I[i]*inv_P[i]))
-update(R[1:n_gen]) <- max(Pmin,R[i] + R_new[i] - (deaths_nat[i]*R[i]*inv_P[i]))
+update(S[1:n_gen]) <- max(Pmin,S[i] - E_new[i] + rbinom(as.integer(P[i]), mu[i]) - rbinom(as.integer(S[i]), mu[i]))
+update(E[1:n_gen]) <- max(Pmin,E[i] + E_new[i] - I_new[i] - rbinom(as.integer(E[i]), mu[i]))
+update(I[1:n_gen]) <- max(Pmin,I[i] + I_new[i] - R_new0[i] - rbinom(as.integer(I[i]), mu[i]))
+update(R[1:n_gen]) <- max(Pmin,R[i] + R_new[i] - rbinom(as.integer(R[i]), mu[i]))
 update(C[1:n_gen]) <- I_new[i]
 update(D[1:n_gen]) <- deaths_YF[i]
 
-#Initial values-----------------------------------------------------------------
+#Initial values of outputs------------------------------------------------------
+initial(time) <- 0
 initial(FOI_total) <- 0
 initial(S[1:n_gen]) <- S_0[i]
 initial(E[1:n_gen]) <- E_0[i]
@@ -70,35 +69,32 @@ initial(R[1:n_gen]) <- R_0[i]
 initial(C[1:n_gen]) <- 0
 initial(D[1:n_gen]) <- 0
 
-#Dimensions---------------------------------------------------------------------
+#Dimensions - inputs------------------------------------------------------------
+dim(temp) <- n_t_pts
+dim(precip) <- n_t_pts
+dim(S_0) <- n_gen
+dim(E_0) <- n_gen
+dim(I_0) <- n_gen
+dim(R_0) <- n_gen
+dim(t_latent) <- n_gen
+dim(t_infectious) <- n_gen
+dim(ifr) <- n_gen
+dim(mu) <- n_gen
+
+#Dimensions - derived values----------------------------------------------------
+dim(rate1) <- n_gen
+dim(rate2) <- n_gen
+dim(E_new) <- n_gen
+dim(I_new) <- n_gen
+dim(R_new0) <- n_gen
+dim(R_new) <- n_gen
+dim(deaths_YF) <- n_gen
+dim(P) <- n_gen
+
+#Dimensions - outputs-----------------------------------------------------------
 dim(S) <- n_gen
 dim(E) <- n_gen
 dim(I) <- n_gen
 dim(R) <- n_gen
 dim(C) <- n_gen
 dim(D) <- n_gen
-
-dim(births) <- n_gen
-dim(deaths_nat) <- n_gen
-dim(E_new) <- n_gen
-dim(I_new) <- n_gen
-dim(R_new0) <- n_gen
-dim(R_new) <- n_gen
-dim(deaths_YF) <- n_gen
-
-dim(P) <- n_gen
-dim(inv_P) <- n_gen
-
-dim(S_0) <- n_gen
-dim(E_0) <- n_gen
-dim(I_0) <- n_gen
-dim(R_0) <- n_gen
-
-dim(temp) <- n_t_pts
-dim(precip) <- n_t_pts
-dim(t_latent) <- n_gen
-dim(t_infectious) <- n_gen
-dim(ifr) <- n_gen
-dim(mu) <- n_gen
-dim(rate1) <- n_gen
-dim(rate2) <- n_gen
